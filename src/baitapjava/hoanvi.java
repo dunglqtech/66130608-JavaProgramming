a82ae275be95aeca4b9d50a2122eef3bba014985
lequangdung1507 <lequangdung1507@gmail.com>
2026-01-31 10:39:55 +0700

    Create hoanvi.java

---
+++ src/baitapjava/hoanvi.java
@@ -0,0 +1,26 @@
+package baitapjava;
+
+import java.util.Scanner;
+
+public class hoanvi {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap so a: ");
+        int a = sc.nextInt();
+
+        System.out.print("Nhap so b: ");
+        int b = sc.nextInt();
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("Sau khi hoan vi:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        sc.close();
+    }
+}
