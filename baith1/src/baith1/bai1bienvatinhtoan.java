38fa120a9c50b6f403a7401571d5cabdc9abad1a
lequangdung1507 <lequangdung1507@gmail.com>
2026-02-03 16:12:18 +0700

    xong bài 1

--- baith1/src/baith1/bai1bienvatinhtoan.java
+++ baith1/src/baith1/bai1bienvatinhtoan.java
@@ -3,8 +3,16 @@ package baith1;
 public class bai1bienvatinhtoan {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			 // 1. Khai báo các biến
+				double a = 15;
+				double  b= 4;
+				double c= 3.5;
+			// 2. Tính toán
+				double tong = a+b;
+				double thuong = a/b;
+			// 3. Xuất ra
+				System.out.println("Tổng là: " + tong);
+				System.out.print("Thương là: " + thuong + "\n");
+			}
 
-	}
-
-}
+		}
