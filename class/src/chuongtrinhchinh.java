db2d47f997affef14d45980ee549e281ee5d5b34
lequangdung1507 <lequangdung1507@gmail.com>
2026-02-09 21:12:43 +0700

    full code

--- class/src/chuongtrinhchinh.java
+++ class/src/chuongtrinhchinh.java
@@ -1,4 +1,12 @@
 
 public class chuongtrinhchinh {
+	public static void main(String[] args) {
+
+		sinhvien sv1 = new sinhvien();
+		sv1.maSV="66130608";
+		sv1.ten="Le Quang Dung";
+		sv1.tuoi=18;
+		sv1.hocBai("lập trình java");
+	}
 
 }
--- class/src/sinhvien.java
+++ class/src/sinhvien.java
@@ -7,6 +7,7 @@ public class sinhvien {
 	    void hocBai(String monHoc) {
 	        System.out.println(ten + " đang học môn " + monHoc);
 	    }
+}
 //	}// Ví dụ: Lớp SinhVien là bản thiết kế
 ////	public class SinhVien {
 //	    String ten;
