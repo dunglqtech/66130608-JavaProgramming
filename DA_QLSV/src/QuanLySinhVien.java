5a6b566a0db1254ee873cba91c6e1676e9280f52
lequangdung1507 <lequangdung1507@gmail.com>
2026-03-17 21:07:54 +0700

    xong class

--- DA_QLSV/src/QuanLySinhVien.java
+++ DA_QLSV/src/QuanLySinhVien.java
@@ -1,3 +1,29 @@
+class SinhVien{
+	String mssv;
+	String ten;
+	int tuoi;
+	String lop;
+	
+	public SinhVien(String mssv, String ten, int tuoi, String lop) {
+		this.mssv = mssv;
+		this.ten = ten;
+		this.tuoi = tuoi;
+		this.lop = lop;
+	}
+	public void hienthi() {
+		System.out.print("MSSV: "+ mssv + "|ten"+ ten + "|tuoi"+ tuoi + "|lop"+ lop);
+	}
+}
+class MonHoc {
+	String MaMH;
+	String TenMH;
+	
+	 public MonHoc(String maMon, String tenMon) {
+	        this.MaMH = maMon;
+	        this.TenMH = tenMon;
+	    }
+	}
+
 
 public class QuanLySinhVien {
 
