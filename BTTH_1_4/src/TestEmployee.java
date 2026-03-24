7cd7b23bdfe7fb3303605303a6cd9dc795c9492b
lequangdung1507 <lequangdung1507@gmail.com>
2026-03-24 23:28:08 +0700

    xong code

--- BTTH_1_4/src/TestEmployee.java
+++ BTTH_1_4/src/TestEmployee.java
@@ -2,8 +2,19 @@
 public class TestEmployee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 Employee e1 = new Employee (8, "Peter", "Tan", 2500);
+         System.out.println(e1);  //toString
+         //Getter and Setter
+         e1.setSalary(999);
+         System.out.println(e1);
+         System.out.println("id là: " + e1.getId());
+         System.out.println("FirstName là: " + e1.getFirstName());
+         System.out.println("LastName là: "+ e1.getLastName());
+         System.out.println("Salary là: " + e1.getSalary());
+         System.out.println("Name là: " + e1.getName());
+         System.out.println("AnnualSalary là: " + e1.getAnnualSalary());
+         //test kiểm tra lương 
+         System.out.println(e1.raiseSalary(10));
+         System.out.println(e1);
 	}
-
 }
