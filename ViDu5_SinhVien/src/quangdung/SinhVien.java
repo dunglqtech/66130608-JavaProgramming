2e575cb01fb09aeb0c0b3d9a01ded36706ee8463
lequangdung1507 <lequangdung1507@gmail.com>
2026-03-31 13:56:50 +0700

    xong lop SinhVien

--- ViDu5_SinhVien/src/quangdung/SinhVien.java
+++ ViDu5_SinhVien/src/quangdung/SinhVien.java
@@ -1,5 +1,41 @@
 package quangdung;
 
 public class SinhVien {
+	//cac thuoc tinh theo yeu cau
+	private String tenHS;
+	private short tuoiHS;
+	private String lopHS;
+	
+	//cac ham tao
+	public SinhVien() {
+		
+	}
+	
+	
 
+	//getter va setter
+	public String getTenHS() {
+		return tenHS;
+	}
+
+	public void setTenHS(String tenHS) {
+		this.tenHS = tenHS;
+	}
+
+	public short getTuoiHS() {
+		return tuoiHS;
+	}
+
+	public void setTuoiHS(short tuoiHS) {
+		this.tuoiHS = tuoiHS;
+	}
+
+	public String getLopHS() {
+		return lopHS;
+	}
+
+	public void setLopHS(String lopHS) {
+		this.lopHS = lopHS;
+	}
+	
 }
