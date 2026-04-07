cf8df4ebddbd993d857f9315215950244bcfda07
lequangdung1507 <lequangdung1507@gmail.com>
2026-04-07 22:09:36 +0700

    xong lop con hinh vuong

--- ViDu6_AbstractClass/src/quangdung/HinhVuong.java
+++ ViDu6_AbstractClass/src/quangdung/HinhVuong.java
@@ -1,5 +1,32 @@
 package quangdung;
 
-public class HinhVuong {
-
+//lop con hinh vuong
+class HinhVuong extends HinhHoc {
+ private double canh;
+ 
+ public HinhVuong(String mauSac, double canh) {
+     super(mauSac);
+     this.canh = canh;
+ }
+ 
+ @Override
+ public double tinhDienTich() {
+     return canh * canh;
+ }
+ 
+ @Override
+ public double tinhChuVi() {
+     return canh * 4;
+ }
+ 
+ @Override
+ public void phongTo(double tyLe) {
+     this.canh *= tyLe;
+     System.out.println("Phóng to hình vuông lên " + tyLe + " lần");
+ }
+ 
+ // phuong thuc rieng
+ public double tinhDuongCheo() {
+     return canh * Math.sqrt(2);
+ }
 }
