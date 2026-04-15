672ba0ba0d6af92aaac35b7aa800396eb1ba2189
lequangdung1507 <lequangdung1507@gmail.com>
2026-04-15 19:36:13 +0700

    xong code đọc dữ liệu từ file

--- ViDu7_StreamReader/src/quangdung/ViDuDocStream.java
+++ ViDu7_StreamReader/src/quangdung/ViDuDocStream.java
@@ -1,26 +1,34 @@
 package quangdung;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ViDuDocStream {
 
 	public static void main(String[] args) throws IOException {
+		FileInputStream fInput = new FileInputStream("tthcm.txt");
+		InputStreamReader ipReader = new InputStreamReader(fInput);
 		//Khai báo biến đối tượng InputStreamReader
-		InputStreamReader ipReader = new InputStreamReader(System.in);
+		//InputStreamReader ipReader = new InputStreamReader(System.in);
 		
 		//Khai báo biến đối tượng buffer 
 		BufferedReader bufReader = new BufferedReader(ipReader);
 		
 		//In một câu hướng dẫn để biết User cần làm gì
-		System.out.println("Nhap mot chuoi bat ki: ");
+		//System.out.println("Nhap mot chuoi bat ki: ");
 				
 		//Đọc dữ liệu từ bàn phím, cất vào biến chuỗi
-		String lineDocDuoc = bufReader.readLine();
+		//String lineDocDuoc = bufReader.readLine();
 		
 		//In ra dòng đọc được
-		System.out.print(lineDocDuoc);
+		//System.out.print(lineDocDuoc);
+		//đọc file
+		String lineDocDuoc1 = bufReader.readLine();
+		
+		//In ra dòng đọc được
+		System.out.print(lineDocDuoc1);
 		
 				
 		
