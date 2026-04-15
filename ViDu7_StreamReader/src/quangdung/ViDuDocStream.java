e64bb1576306bc0aa1cabca5789726e7f0e39b6c
lequangdung1507 <lequangdung1507@gmail.com>
2026-04-15 19:22:49 +0700

    xong code doc tu cach nhap ban phim

--- ViDu7_StreamReader/src/quangdung/ViDuDocStream.java
+++ ViDu7_StreamReader/src/quangdung/ViDuDocStream.java
@@ -1,10 +1,29 @@
 package quangdung;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ViDuDocStream {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		//Khai báo biến đối tượng InputStreamReader
+		InputStreamReader ipReader = new InputStreamReader(System.in);
+		
+		//Khai báo biến đối tượng buffer 
+		BufferedReader bufReader = new BufferedReader(ipReader);
+		
+		//In một câu hướng dẫn để biết User cần làm gì
+		System.out.println("Nhap mot chuoi bat ki: ");
+				
+		//Đọc dữ liệu từ bàn phím, cất vào biến chuỗi
+		String lineDocDuoc = bufReader.readLine();
+		
+		//In ra dòng đọc được
+		System.out.print(lineDocDuoc);
+		
+				
+		
 	}
 
 }
