fabedd529b7582237316e5711b3d7d79a54b8fbd
lequangdung1507 <lequangdung1507@gmail.com>
2026-04-23 07:40:50 +0700

    Lan thu 1

---
+++ ThiGiuaKi-Bai1/src/thigk2/LeQuangDung/ChuNhat.java
@@ -0,0 +1,27 @@
+package thigk2.LeQuangDung;
+
+import java.util.Scanner;
+
+public class ChuNhat {
+	
+	//cach lam
+	//tao cho chuong trinh bat nguoi dung nhap 2 diem (x1,y1) vaf (x2,y2) cua hinh chu nhat
+	//tinh chieu dai = |x2-x1|, chieu rong |y2-y1|
+	//tinh chu vi = 2*(dai + rong)
+	//tinh dien tich = dai * rong
+
+	public static void main(String[] args) {
+		//tao lop scanner
+		Scanner banphim = new Scanner(System.in);
+		
+		//nhap du lieu goc toa to tren-trai
+		System.out.print("nhap toa do cua diem x1: ");
+		double x1 = banphim.nextDouble();
+		System.out.print("nhap toa do diem y1: ");
+		double y1 = banphim.nextDouble();
+		
+		//nhap du
+
+	}
+
+}
