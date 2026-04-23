4db4d12453d0ac6eb70ad504d7ae30a2d66eea4f
lequangdung1507 <lequangdung1507@gmail.com>
2026-04-23 08:11:35 +0700

    lan thu 5

---
+++ ThiGiuaKi-Cau2/src/thigk2/LeQuangDung/Run.java
@@ -0,0 +1,29 @@
+package thigk2.LeQuangDung;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Run {
+	
+	//cach lam:
+	//tao lop SanPham gom ma, ten loai, gia
+	//ham main:
+	//tao danh sach arraylist
+	//hard-code 3 san pham ban dau va in ra danh sach do
+	//them 1 san pham vao trong danh sach(add) va nhap thong tin tu ban phim
+	//in ra laij man hinh danh sach san pham
+	//in ra man hinh danh sach cac san pham co loaij la "thuc pham chuc nang"
+	
+
+	public static void main(String[] args) {
+		//tao lop scanner
+		Scanner banphim = new Scanner(System.in);
+		//tao arraylist
+		ArrayList<SanPham> ds = new ArrayList<>();
+		
+		//hard-code 3 san pham
+		
+
+	}
+
+}
