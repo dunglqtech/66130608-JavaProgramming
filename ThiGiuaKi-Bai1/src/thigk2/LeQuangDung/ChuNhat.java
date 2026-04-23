21fe7cf58c2920a7eeecdee00fdd1d8ae779cbf1
lequangdung1507 <lequangdung1507@gmail.com>
2026-04-23 07:47:46 +0700

    Lan thu 2

--- ThiGiuaKi-Bai1/src/thigk2/LeQuangDung/ChuNhat.java
+++ ThiGiuaKi-Bai1/src/thigk2/LeQuangDung/ChuNhat.java
@@ -14,13 +14,31 @@ public class ChuNhat {
 		//tao lop scanner
 		Scanner banphim = new Scanner(System.in);
 		
-		//nhap du lieu goc toa to tren-trai
+		//nhap du lieu goc toa do tren-trai
 		System.out.print("nhap toa do cua diem x1: ");
 		double x1 = banphim.nextDouble();
 		System.out.print("nhap toa do diem y1: ");
 		double y1 = banphim.nextDouble();
 		
-		//nhap du
+		//nhap du lieu goc toa do duoi-phai
+		System.out.print("nhap toa do cua diem x2: ");
+		double x2 = banphim.nextDouble();
+		System.out.print("nhap toa do diem y2: ");
+		double y2 = banphim.nextDouble();
+		
+		//cong thuc tinh chieu dai
+		double ChieuDai = Math.abs(x2-x1);
+		double ChieuRong = Math.abs(y2-y1);
+		
+		//tinh chu vi hinh chu nhat
+		double ChuVi = 2 * (ChieuDai + ChieuRong);
+		
+		//tinh dien tichs
+		double DienTich = ChieuDai * ChieuRong;
+		
+		//in ra man hinh ket qua
+		System.out.print("Chu vi cua hinh chu nhat la: " + ChuVi);
+		System.out.print("dien tich cua hinh chu nhat la: " + DienTich);
 
 	}
 
